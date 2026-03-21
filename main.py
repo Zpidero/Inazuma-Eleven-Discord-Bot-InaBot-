52dbd92d1e93224c31541af1a263e1b285e72d09
Lluc Vivet Torras <lluc@vivet.cat>
2026-03-21 15:25:45 +0100

    change bot prefix default

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ load_dotenv()
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix="!", intents=intents)
+bot = commands.Bot(command_prefix="$$$", intents=intents)
 
 
 API_URL = os.getenv("API_URL", "https://inazumaeleven-api.onrender.com") # Cambiar aixo si la API está en un altre lloc
